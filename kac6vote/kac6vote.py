c4d93c20d1a8124560967a6a580ef4a4cacc5f0c
Unknown <7pairs@gmail.com>
2017-06-04 13:50:28 +0900

    Update kac6vote.py #8

--- kac6vote/kac6vote.py
+++ kac6vote/kac6vote.py
@@ -31,6 +31,9 @@ Options:
   -v --version  print the version number and exit.
 """
 
+from __future__ import print_function
+from __future__ import unicode_literals
+
 import codecs
 import collections
 import itertools
@@ -42,9 +45,8 @@ import bs4
 import docopt
 import requests
 
+from . import __version__
 
-# バージョン番号
-VERSION = '1.0.0'
 
 # 投票ページのURL
 VOTE_PAGE_URL = 'http://p.eagate.573.jp/game/qma/12/p/qt/setkac.html'
@@ -56,15 +58,15 @@ CONSOLE_ENCODE = 'shift_jis' if sys.platform == 'win32' else 'utf-8'
 WAIT_AFTER_VOTE = 1
 
 
-# 21世紀になって16年が過ぎたというのにまさかのShift_JIS対応
+# 21世紀に入って16年が過ぎたというのにまさかのShift_JIS対応
 def windows31j(name):
     """
     Windows-31J用の検索関数。
-    厳密には一致しないが、仕方なくShift_JISにマッピングする。
+    厳密には同一のコードではないが、近いものとしてShift_JISにマッピングする。
 
     :param name: コーデック名
     :type name: str
-    :return: Windows-31Jが指定された場合はShift_JIS, それ以外の場合はNone
+    :return: Windows-31Jが指定された場合はShift_JIS、それ以外の場合はNone
     :rtype: codecs.CodecInfo
     """
     # Windows-31JをShift_JISにマッピングする
@@ -84,10 +86,10 @@ Player = collections.namedtuple('Player', ['name', 'value'])
 def main():
     """
     「QMAジャパンツアー2016 グランドスラムダービー」のすべての組み合わせに投票する。
-    スクリプトでは画像認証を突破できないので、ブラウザのCookieを利用してログインする。
+    eAMUSEMENTにはブラウザのCookieを利用してログインするため、実行前にブラウザでのログインが必要。
     """
     # Cookie取得対象のブラウザを決定する
-    args = docopt.docopt(__doc__, version=VERSION)
+    args = docopt.docopt(__doc__, version=__version__)
     if args.get('-b') == 'firefox':
         cookie_jar = browsercookie.firefox()
     else:
@@ -100,12 +102,12 @@ def main():
     soup = bs4.BeautifulSoup(response.text.encode(response.encoding), 'html.parser')
     player_name_box = soup.find('div', {'class': 'player_name_box'})
     if not player_name_box:
-        print u'指定のブラウザでログインしてから実行してください'
+        print('ブラウザでログインしてから実行してください。')
         exit()
     player_name = player_name_box.text.strip()
 
     # 確認メッセージを表示する
-    message = player_name + u'さんのアカウントで投票します (Y/n) : '
+    message = player_name + 'さんのアカウントで投票します (y/n) : '
     if raw_input(message.encode(CONSOLE_ENCODE)).strip().lower() != 'y':
         exit()
 
@@ -116,12 +118,12 @@ def main():
 
     # すべての組み合わせに投票する
     for quinella in itertools.combinations(players, 2):
-        print quinella[0].name, '-', quinella[1].name, u'に投票しています...'
+        print(quinella[0].name, '-', quinella[1].name, 'に投票しています...')
         requests.post(VOTE_PAGE_URL, {'vote0': quinella[0].value, 'vote1': quinella[1].value}, cookies=cookie_jar)
         time.sleep(WAIT_AFTER_VOTE)
 
     # 正常終了
-    print u'投票が完了しました！'
+    print('投票が完了しました！')
 
 
 # メイン処理を呼び出す
