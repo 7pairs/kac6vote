0b74457330cb348ceb073504c32822a28c0778cd
HASEBA Junya <7pairs@gmail.com>
2019-08-21 17:53:37 +0900

    Update __init__.py #21

--- kac6vote/__init__.py
+++ kac6vote/__init__.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 #
-# Copyright 2017-2019 Jun-ya HASEBA
+# Copyright 2017-2019 HASEBA Junya
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
